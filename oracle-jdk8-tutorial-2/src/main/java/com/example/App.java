71543ad8aee2a3e07b80089a9d8c3af59c25c14c
Youssef <youssefbelhaj7328@gmail.com>
2026-06-02 18:13:13 +0200

    Variable resultado  eliminada de metodo infoDiaSemana

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -555,24 +555,23 @@ public class App {
 	}
 
 	String infoDiaSemana(DiaSemana diaSemana) {
-		String resultado = null;
-
+		
 		if (diaSemana.equals(DiaSemana.LUNES))
-			resultado = "Primer dia de la semana, a trabajar";
+			return "Primer dia de la semana, a trabajar";
 		else if (diaSemana.equals(DiaSemana.MARTES))
-			resultado = "Hemos pasado el lunes";
+			return "Hemos pasado el lunes";
 		else if (diaSemana.equals(DiaSemana.MIERCOLES))
-			resultado = "Nos acercamos al fin de semana";
+			return "Nos acercamos al fin de semana";
 		else if (diaSemana.equals(DiaSemana.JUEVES))
-			resultado = "Es juernes, los nuevos viernes, a disfrutar!!!";
+			return "Es juernes, los nuevos viernes, a disfrutar!!!";
 		else if (diaSemana.equals(DiaSemana.VIERNES))
-			resultado = "El viernes y el cuerpo lo sabe";
+			return "El viernes y el cuerpo lo sabe";
 		else if (diaSemana.equals(DiaSemana.SABADO) || diaSemana.equals(DiaSemana.DOMINGO))
-			resultado = "Es fin de semana";
+			return "Es fin de semana";
 		else
-			resultado = "El dia recibido no existe";
+			return "El dia recibido no existe";
 
-		return resultado;
+		
 
 	}
 }
